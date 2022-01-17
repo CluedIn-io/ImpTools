a5ad7898e347be30ed1b99bdb4f2643efbfa0801
Rudi Bob <33795220+RudiBob@users.noreply.github.com>
2022-01-17 11:00:03 +1100

    improved metadata comments and disabled option c

--- docker/ubuntu/manage_cluedin.sh
+++ docker/ubuntu/manage_cluedin.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# simple script for managing common CluedIn functions
-# tested with 20.04.3 LTS (Focal Fossa) in Azure on a D8s_v3 VM using Ubuntu Server 20.04 LTS - Gen1
-# rha@cluedin.com Nov 2021
+# purpose: simple script for managing common CluedIn functions with Docker Desktop on Ubuntu
+# Nov 2021 - tested with 20.04.3 LTS (Focal Fossa) in Azure on a F16s_v2 preferred (D8s_v3 also works but not as well) VM using Ubuntu Server 20.04 LTS - Gen1
+# author: rha@cluedin.com
+# version: 0.2
 
 function menu()
 {
@@ -16,7 +17,7 @@ function menu()
 	echo "u - up - start and ensuring changed containers are created"
 	echo "w - wipe i.e. tear DOWN cluster and bring it back UP"
 	echo "d - wipe/down i.e. tear DOWN cluster"
-	echo "c - create new org and admin user"
+	#echo "c - create new org and admin user"
 	echo "q - to quit"
 }
 
@@ -29,7 +30,7 @@ function anykey()
 pushd .
 
 tag=325
-cd /home/azureuser/Home/src
+cd /home/azureuser/Home
 
 while [ true ]
 do
@@ -61,9 +62,9 @@ do
 	"d")
 		sudo pwsh ./cluedin.ps1 down $tag
 	;;
-	"c")
-		echo "If you really want this feature let Rudi know..."
-	;;
+#	"c")
+#		echo "If you really want this feature let Rudi know..."
+#	;;
 	"q")
 		break
 	;;
