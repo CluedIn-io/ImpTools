db471006e2fbff617b7ddafd663ffd5071e7c272
“RudiBob” <rudi.bob.harris@gmail.com>
2022-01-20 17:47:51 +1000

    minor commenting improvements

--- dotnet-dump/dump_all.sh
+++ dotnet-dump/dump_all.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# first arg is the location of the crashdumps to recursively scan
+# run this inside of the docker container running dotnet sdk
+# run from:
+#   cd /dumps
+# first arg to this script is the location of the crashdumps to recursively scan
 
 find $1 -name coredump.? -exec ./dotnet-dump-clrstack.sh {} \;
 find $1 -name coredump.?? -exec ./dotnet-dump-clrstack.sh {} \;
--- dotnet-dump/install_dotnet_dump.sh
+++ dotnet-dump/install_dotnet_dump.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# run this inside of the docker container running dotnet sdk
 dotnet tool install -g dotnet-dump
