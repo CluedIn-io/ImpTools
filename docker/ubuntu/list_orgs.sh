1f028796d9e3042e19956a8b69fa92e04a95bd40
Rudi Bob <33795220+RudiBob@users.noreply.github.com>
2022-02-21 18:05:52 +1100

    Update list_orgs.sh
    added instructions on how to get sqlcmd in your environment

--- docker/ubuntu/list_orgs.sh
+++ docker/ubuntu/list_orgs.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# list all the organizations in the local docker mssql
+# install sqlcmd first using steps from https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools?view=sql-server-ver15#ubuntu
+# 
+# TLDR; version
+# curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+#	sudo apt-get update 
+#	sudo apt-get install mssql-tools unixodbc-dev
+#	sudo apt-get update 
+#	sudo apt-get install mssql-tools
+#	Optional: Add /opt/mssql-tools/bin/ to your PATH environment variable in a bash shell.
+# export PATH=:$PATH:/opt/mssql-tools/bin
+
 
 # access local docker mssql
 DBUSER='sa'
