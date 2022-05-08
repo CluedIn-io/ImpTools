be90b5452e73669ed786106c385f20d21bea5913
“RudiBob” <rudi.bob.harris@gmail.com>
2022-05-09 00:07:56 +1000

    how to run doxygen info added to ps1 script

--- doxygen/docker_run_doxygen.ps1
+++ doxygen/docker_run_doxygen.ps1
@@ -1,1 +1,4 @@
+Write-Host "See https://github.com/CluedIn-io/ImpTools/tree/master/doxygen"
+Write-Host "Run this command in the docker container cli and wait 10 to 15 minutes"
+Write-Host "cd /src && doxygen"
 docker run -it --rm --name CluedIn.Doxygen -v ${pwd}:/src starlabio/doxygen
