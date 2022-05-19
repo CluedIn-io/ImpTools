97847b696b3b78b612772ebb53656b88a4e881ba
“RudiBob” <rudi.bob.harris@gmail.com>
2022-05-20 09:06:11 +1000

    updated sizing information for reference purposes

---
+++ k8s/pwsh/dev-sizing/helm_upgrade-3.3.1.ps1
@@ -0,0 +1,5 @@
+helm upgrade cluedin-demo cluedin/cluedin-platform `
+  -n cluedin `
+  --install `
+  --version 1.0.2 `
+  --values values.3.3.1.cluedin-platform-1.0.2.yml
---
+++ k8s/pwsh/prd-medium-sizing/helm_upgrade-3.3.0.ps1
@@ -0,0 +1,5 @@
+helm upgrade cluedin-demo cluedin/cluedin-platform `
+  -n cluedin `
+  --install `
+  --version 1.0.0 `
+  --values values.3.3.0.cluedin-platform-1.0.0.yml
